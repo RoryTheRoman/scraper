d1ef9f50b50ee56f95d666f5b9a63f6564765ef6
Mike D <michael.mcdaniel210@gmail.com>
2018-04-05 20:50:20 -0500

    added code for comment modal

--- controllers/scraper_controller.js
+++ controllers/scraper_controller.js
@@ -13,19 +13,23 @@ module.exports = function (app) {
     app.get("/scrape", function (req, res) {
         axios.get("https://www.theonion.com/").then(function (response) {
             var $ = cheerio.load(response.data);
-            $("h1.headline").each(function (i, element) {
+            $("article.postlist__item").each(function (i, element) {
                 var result = {};
 
-                result.title = $(element).children("a").text().trim();
-                // console.log(result.title);
+                result.title = $(element).children("header").children("h1.headline").children("a").text().trim();
+                console.log(result.title);
 
-                result.link = $(element).children("a").attr("href").trim();
-                // console.log(result.link);
+                result.link = $(element).children("header").children("h1.headline").children("a").attr("href").trim();
+                console.log(result.link);
+                result.summary = $(element).children("div.item__content").children("div.excerpt").children("p").text().trim();
+
+
+                console.log(result.summary);
 
                 db.Article.create(result)
                     .then(function (dbArticle) {
                         // View the added result in the console
-                        console.log(dbArticle);
+                        // console.log(dbArticle);
                     })
                     .catch(function (err) {
                         // If an error occurred, send it to the client
